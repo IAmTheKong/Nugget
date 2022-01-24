34efa71cc7757c05253f386236d19b45dcbbf9b3
Jakub <nuggettothemoon@gmail.com>
2022-01-24 18:00:30 +0100

    4.01

---
+++ Nugget/index.js
@@ -0,0 +1,10 @@
+var currency1 = document.getElementById("currency-1").innerHTML
+var currency2 = document.getElementById("currency-2").innerHTML
+
+
+function currencyFunction() {
+    let currency = document.getElementsByClassName("currency").innerHTML
+    document.getElementById("currency-dd").innerHTML = currency
+}
+
+
